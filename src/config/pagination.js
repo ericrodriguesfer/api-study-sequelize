5212bdc39315610b6c5bf729ad34c2b5384441c4
Eric Rodrigues <er601432@gmail.com>
2022-07-17 21:46:57 -0300

    fix: fixing types return on pagination methos

--- src/config/pagination.js
+++ src/config/pagination.js
@@ -1,15 +1,15 @@
 function getPagination(page, size) {
-  const limit = size ? size : 3;
-  const offset = page ? page * limit : 0;
+  const limit = size ? parseInt(size) : 3;
+  const offset = page ? parseInt(page) * limit : 0;
 
   return { limit, offset };
 }
 
 function getPagingData(data, page, limit) {
   const { count: totalItems, rows: datas } = data;
-  const currentPage = page ? page : 0;
+  const currentPage = page ? parseInt(page) : 0;
   const totalPages = Math.ceil(totalItems / limit);
-  return { totalItems, datas, totalPages, currentPage };
+  return { totalItems, data: datas, totalPages, currentPage };
 }
 
 export { getPagination, getPagingData };
